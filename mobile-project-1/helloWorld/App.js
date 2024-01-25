7e72cd0cc07b4ad61d7ff819e285d201f5f4618f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-25 08:59:43 -0300

    Criado Style para texto e personalização

--- mobile-project-1/helloWorld/App.js
+++ mobile-project-1/helloWorld/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Word!</Text>
+      <Text style={styles.texto}>Hello Word!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texto: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 500,
   },
 });
