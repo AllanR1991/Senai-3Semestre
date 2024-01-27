c297c603f1dc033530bef906555cc73d5d7de11f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-26 21:26:39 -0300

    Corrigindo sistema

--- mobile-project/epicGames/App.js
+++ mobile-project/epicGames/App.js
@@ -1,58 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image
-        style={styles.logo}
         source={require('./src/assets/image/epic.png')}
+        style={styles.logo}      
       />
-      <Text
-        style={styles.titulo}
-      >
-        Login
-      </Text>
-      <View
-        style={styles.containerInputs}
-      >
-        <View
-          style={styles.containerInput}
-        >
+      <Text style={styles.titulo}>Login</Text>
+      <View style={styles.containerInputs}>
+        <View style={styles.containerInput}>
           {/* <Text
             style={styles.rotulo}
           >Email</Text> */}
           <TextInput
             style={styles.input}
-            placeholder='Email'
-          >
-          </TextInput>
+            placeholder="email@domain.com"
+            placeholderTextColor="white"
+            autoComplete="off"
+            inputMode="email"
+            autoCapitalize="none"
+          ></TextInput>
         </View>
 
-        <View
-          style={styles.containerInput}
-        >
+        <View style={styles.containerInput}>
           {/* <Text
             style={styles.rotulo}
           >Senha</Text> */}
           <TextInput
             secureTextEntry={true}
+            placeholder="Senha"
+            autoComplete="off"
+            selectionColor="white"
+            inputMode="text"
+            placeholderTextColor="white"
             style={styles.input}
-            placeholder='Senha'
-          >
-          </TextInput>
+          ></TextInput>
         </View>
       </View>
 
-      <TouchableOpacity 
-        style={styles.btn}
-      >
-        <Text 
-          style={styles.textBtn}
-        >
-          Entrar
-        </Text>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textBtn}>Entrar</Text>
       </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -61,10 +60,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap:38,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 38,
     padding: 18,
   },
   logo: {
@@ -73,36 +72,34 @@ const styles = StyleSheet.create({
   },
   titulo: {
     fontSize: 36,
-    textShadowColor:  '#000',
-    textShadowOffset: {width:2,height:2},
-    textShadowRadius: 6.2
+    textShadowColor: "#000",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 6.2,
   },
   rotulo: {
-    fontSize: 16
+    fontSize: 16,
   },
   input: {
-    backgroundColor: '#818181',
-    color: 'white',
+    backgroundColor: "#818181",
+    color: "white",
     borderRadius: 45,
     padding: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
   containerInput: {
-    gap: 10
+    gap: 10,
   },
-  containerInputs:{
-    gap:10,
-    width: '100%',
-    maxWidth: 450
+  containerInputs: {
+    gap: 10,
+    width: "100%",
+    maxWidth: 450,
   },
-  btn:{
-    backgroundColor: 'black',
+  btn: {
+    backgroundColor: "black",
     padding: 16,
     borderRadius: 17,
-    
   },
-  textBtn:{
-    color: 'white',
-    
-  }
+  textBtn: {
+    color: "white",
+  },
 });
