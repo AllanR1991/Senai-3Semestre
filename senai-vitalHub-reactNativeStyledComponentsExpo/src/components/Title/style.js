7bbde52d8641618d3aba0d98f2907dfa2b5a7524
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-02-15 16:35:43 -0300

    removido

--- senai-vitalHub-reactNativeStyledComponentsExpo/src/components/Title/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-
-export const TitleStyle = styled.Text`
-  font-size: 20px;
-  color: #33303E;
-  font-family: 'MontserratAlternates_600SemiBold';
-  margin-bottom: ${props => `${props.marginBottom}px`};
-  margin-top: ${props => `${props.marginTop}px`};
-`
