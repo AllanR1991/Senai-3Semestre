17739df19690e196fa16ec8c58c4d584f319e190
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-07 07:58:02 -0300

    Atualizando projeto VitalHub

--- senai-virtualHub-reactNative/src/components/DoctorCard/Style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ImageDoctorProfile = styled.Image`
-    width: 77px;
-    height: 80px;
-    border-radius: 5px;
-`
--- senai-virtualHub-reactNative/src/screens/VerifyEmail/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const ContainerBanner = styled.View`
-    flex-direction: row;
-    margin-top: 20px;
-    margin-bottom: 25px;
-    width: 100%;
-`
