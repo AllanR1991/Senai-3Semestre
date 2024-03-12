27555e745d4ae5d28928572a18770e49dee31cab
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-12 10:29:34 -0300

    Atualizado VitalHub

---
+++ VitalHub/src/components/Map/index.js
@@ -0,0 +1,288 @@
+/* importando navegação  */
+
+import Mapview, {Maker, Marker, PROVIDER_GOOGLE} from   'react-native-maps'
+import { useEffect, useState } from "react"
+
+import {
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,    
+} from 'expo-location'
+import { View } from 'react-native'
+
+export const Map = ({navigation}) => {
+
+
+    const clinica = [{ latitude:-23.69831073189791, longitude: -46.5473883224051}]
+    // Criando UseState para setar localização inicial e destino.9
+    // Destino deve ser passado como props pois cada clinica tem um destino.
+    const [initialPosition, setInitialPosition] = useState(null);
+    const [finalPosition, setFinalPosition] = useState({
+        latitude: clinica[1].latitude ,
+        longitude: clinica[1].latitude 
+    })
+
+    // Captura Localização
+    async function CurrentLocation(){
+        /* Verifica se a permissão foi garantida ou seja se foi autorizado. */
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if(granted){
+            // Se permissao for autorizada obtemos a localização atual.
+            const captureLocation = await getCurrentPositionAsync();
+        }
+    }
+
+    useEffect(()=> {
+        CurrentLocation();
+    },[1000])
+
+    return(
+      <View>
+        <Mapview
+        //Definindo Ponto inicial
+          initialRegion={{
+            latitude: initialPosition.coords.latitude,
+            longitude: initialPosition.coors.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+          // Especifica o provedor de dados.
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={grayMapStyle}
+          style={{
+            flex:1,
+            width: '100%',
+          }}
+        >
+        
+
+        </Mapview>
+      </View>
+    )
+}
+
+
+const grayMapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#E1E0E7",
+      },
+    ],
+  },
+  {
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        saturation: -5,
+      },
+      {
+        lightness: -5,
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#33303E",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#66DA9F",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1B1B1B",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#C6C5CE",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ACABB7",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8EA5D9",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+];
+
+
