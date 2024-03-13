21d08315d524ae3719405f8455cf9bd368ae42f5
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-13 09:57:11 -0300

    Maps

--- VitalHub/src/components/MapLocation/index.js
+++ VitalHub/src/components/MapLocation/index.js
@@ -1,68 +1,179 @@
 /* importando navegação  */
 
-import Mapview, {Maker, Marker, PROVIDER_GOOGLE} from   'react-native-maps'
-import { useEffect, useState } from "react"
+import Mapview, 
+      { Maker,
+        PROVIDER_GOOGLE } from 'react-native-maps'
+        
+import { useEffect, useRef, useState } from "react"
 
 import {
-    requestForegroundPermissionsAsync,
-    getCurrentPositionAsync,    
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  watchPositionAsync,
+  LocationAccuracy
 } from 'expo-location'
-import { View } from 'react-native'
 
-export const MapLocation = ({navigation}) => {
+import { 
+  ActivityIndicator, 
+  StyleSheet, 
+  Text, 
+  View } from 'react-native'
+import MapViewDirections from 'react-native-maps-directions'
+import { mapskey } from '../../utils/mapsApiKey'
 
+export const MapLocation = ({ navigation }) => {
 
-    const clinica = [{ latitude:-23.6983, longitude: -46.5473}]
-    // Criando UseState para setar localização inicial e destino.9
-    // Destino deve ser passado como props pois cada clinica tem um destino.
-    const [initialPosition, setInitialPosition] = useState(null);
-    const [finalPosition, setFinalPosition] = useState({
-        latitude: clinica[1].latitude ,
-        longitude: clinica[1].longitude 
-    })
+  const mapReference = useRef(null);
 
-    // Captura Localização
-    async function CurrentLocation(){
-        /* Verifica se a permissão foi garantida ou seja se foi autorizado. */
-        const { granted } = await requestForegroundPermissionsAsync();
+  const clinica = [{ latitude: -23.6983, longitude: -46.5473, clinica: 'Health Clinic' }]
+  // Criando UseState para setar localização inicial e destino.9
+  // Destino deve ser passado como props pois cada clinica tem um destino.
+  const [initialPosition, setInitialPosition] = useState(null);
+  const [finalPosition, setFinalPosition] = useState({
+    latitude: clinica[0].latitude,
+    longitude: clinica[0].longitude
+  })
 
-        if(granted){
-            // Se permissao for autorizada obtemos a localização atual.
-            const captureLocation = await getCurrentPositionAsync();
+  // Captura Localização
+  async function CurrentLocation() {
+    /* Verifica se a permissão foi garantida ou seja se foi autorizado. */
+    const { granted } = await requestForegroundPermissionsAsync();
+    //console.log(granted);
+    if (granted) {
+      // Se permissao for autorizada obtemos a localização atual.
+      const captureLocation = await getCurrentPositionAsync();
+      //console.log(captureLocation)
 
-            // Define a localizaçao inicial
-            setInitialPosition(captureLocation);
-        }
+      // Define a localizaçao inicial
+      setInitialPosition(captureLocation);
     }
+  }
 
-    useEffect(()=> {
-        CurrentLocation();
-    },[1000])
 
-    return(
-      <View>
-        <Mapview
-        //Definindo Ponto inicial
-          initialRegion={{
-            latitude: initialPosition.coords.latitude,
-            longitude: initialPosition.coors.longitude,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005
-          }}
-          // Especifica o provedor de dados.
-          provider={PROVIDER_GOOGLE}
-          customMapStyle={grayMapStyle}
-          style={{
-            flex:1,
-            width: '100%',
-          }}
-        >
-        
 
-        </Mapview>
-      </View>
+
+  useEffect(() => {
+    CurrentLocation();
+    //monitora a posiçao em tempo real
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      async (response) => {
+        // Recebe e guarda a nova posiçao
+        await setInitialPosition(response)
+      }
     )
+  }, [1000])
+
+
+  useEffect(() => {
+    recarregarVizualizacaoMapa()
+  }, [initialPosition])
+
+  async function recarregarVizualizacaoMapa() {
+    if (mapReference.current && initialPosition) {
+      await mapReference.current.fitToCoordinates(
+        [{
+          latitude: initialPosition.coords.latitude,
+          longitude: initialPosition.coords.longitude
+        },
+        {
+          latitude: finalPosition.latitude,
+          longitude: finalPosition.longitude,
+        }], {
+        edgePadding: { top: 60, rigth: 60, bottom: 60, left: 60 },
+        animated: true,
+      })
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+
+      {
+        initialPosition != null ?
+          <Mapview
+            ref={mapReference}
+            //Definindo Ponto inicial -23.745093706934128, -46.53144640134541
+            initialRegion={{
+              latitude: initialPosition.coords.latitude,
+              longitude: initialPosition.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005
+            }}
+            // Especifica o provedor de dados.
+            provider={PROVIDER_GOOGLE}
+            // Estilizaçao do mapa
+            customMapStyle={grayMapStyle}
+
+            style={{
+              height: 200,
+              width: '100%',
+            }}
+          >
+            {/* Definindo ponto indicando onde voce se encontra. */}
+            <Marker
+              coordinate={{
+                latitude: initialPosition.coords.latitude,
+                longitude: initialPosition.coords.longitude,
+              }}
+              title='Voce está aqui.'
+              description='Posição inicial'
+              pinColor='green'
+            />
+            {/* Definindo ponto da clinica */}
+            <Marker
+              coordinate={{
+                latitude: clinica[0].latitude,
+                longitude: clinica[0].longitude,
+                LatitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }}
+              title={clinica[0].clinica}
+              pinColor='blue'
+            />
+            {/* Rota entre os dois ponto */}
+            <MapViewDirections
+              origin={initialPosition.coords}
+              destination={{
+                latitude: clinica[0].latitude,
+                longitude: clinica[0].longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }}
+              strokeWidth={5}
+              strokeColor='#496BBA'
+              apikey={mapskey}
+
+            />
+
+
+          </Mapview> :
+
+          <>
+            <Text>Localizacão nao encontrado</Text>
+            <ActivityIndicator />
+          </>
+
+
+      }
+
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 200,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 
 const grayMapStyle = [
